b6143b6fd7a76ae50069c07309d555314ce34674
royverhoeven8-dotcom <royverhoeven8@gmail.com>
2026-06-12 11:36:17 +0100

    ff kijken

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,7 +5,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js').catch(() => {});
+    navigator.serviceWorker.register('./sw.js').catch(() => {});
   });
 }
    /*test*/
